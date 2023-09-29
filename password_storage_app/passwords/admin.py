ae4d3dab5e45d9c50bc37a06ad8f4b293ecfd15a
lisaGDev <136374063+lisaGDev@users.noreply.github.com>
2023-09-29 13:14:13 -0400

    Delete password_storage_app directory

--- password_storage_app/passwords/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Password
-
-# Register your models here.
-
-class MemberAdmin(admin.ModelAdmin):
-  list_display = ("id", "data_created", "time", "app_name", "username", "password", "type")
-  
-admin.site.register(Password, MemberAdmin)
